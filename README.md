13c283b73e77860f5377f686e800cab5964b8392
Haruki Kondo <avp.104.106.107@ae.auone-net.jp>
2024-09-20 12:30:34 +0000

    テンプレを追加

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Ginco-One-Platform-Sample
+
 This is a repo for Ginco-One-Platform-Sample.
+
+## How to work
+
+### 参考文献
+
+1. [Ginco One Platform Site](https://console.cloud.ginco.com/nodes/api)
+   > > > > > > > 32d04b3 (テンプレを追加)
---
+++ hardhat.config.ts
@@ -0,0 +1,47 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+import fs from "fs";
+import path from "path";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const { PRIVATE_KEY, GINCO_API_KEY } = process.env;
+
+// タスクファイルを読み込むための設定
+const SKIP_LOAD = process.env.SKIP_LOAD === "true";
+if (!SKIP_LOAD) {
+	const taskPaths = ["", "utils"];
+	taskPaths.forEach((folder) => {
+		const tasksPath = path.join(__dirname, "tasks", folder);
+		fs.readdirSync(tasksPath)
+			.filter((_path) => _path.includes(".ts"))
+			.forEach((task) => {
+				require(`${tasksPath}/${task}`);
+			});
+	});
+}
+
+const config: HardhatUserConfig = {
+	solidity: {
+		compilers: [
+			{
+				version: "0.8.27",
+				settings: {
+					viaIR: true,
+				},
+			},
+		],
+	},
+	networks: {
+		hardhat: {
+			allowUnlimitedContractSize: true,
+		},
+		holesky: {
+			url: `https://testnet.node.gincoapis.com/ethereum/holesky/v1/${GINCO_API_KEY}`,
+			accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+		},
+	},
+};
+
+export default config;
